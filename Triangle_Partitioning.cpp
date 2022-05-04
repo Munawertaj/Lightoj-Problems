603705d99bbf1f7c42c6f458b9a467815caef8fd
Munawer Taj <taj4580@gmail.com>
2022-05-04 21:38:18 +0600

    LOJ-1043

---
+++ Triangle_Partitioning.cpp
@@ -0,0 +1,50 @@
+//  https://lightoj.com/problem/triangle-partitioning
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define matrix(x) vector<vector<x>>
+#define ff first
+#define ss second
+#define ms(a, b) memset(a, b, sizeof(a))
+#define PI (2 * acos(0.0))
+#define mod 1000000007
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define INF (ll)1e17
+#define nl endl
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    double ab, ac, bc, ratio, AD;
+    cin >> ab >> ac >> bc >> ratio;
+
+    AD = ab * sqrt(ratio / (ratio + 1));
+
+    cout << fixed << setprecision(7) << AD << nl;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+
+        cout << "Case " << i << ": ";
+        solve();
+    }
+    return 0;
+}
